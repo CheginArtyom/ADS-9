65f32cfc6070581155c97b5a8f0725c833ebd0b6
CheginArtyom <99319275+CheginArtyom@users.noreply.github.com>
2022-06-10 22:53:22 +0300

    Create alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,67 +1,30 @@
 // Copyright 2021 CheginArtyom
-#ifndef INCLUDE_BST_H_
-#define INCLUDE_BST_H_
-template<typename T>
-class BST {
-  public:
-  BST() :kor(nullptr) {}
-  void Plus(const T& znc) {
-    kor = plusvetka(kor, znc);
-  }
-  int poisk(const T& znc) {
-    return poiskvetka(kor, znc);
-  }
-  int d() {
-    return dp(kor) - 1;
-  }
-  
- private:
-  struct vetka {
-  T zn;
-  int c = 0;
-  vetka* levo = nullptr;
-  vetka* pravo = nullptr;;
-  };
-  vetka* kor;
-  vetka* plusvetka(vetka* kor, const T& znc) {
-    if (kor == nullptr) {
-      kor = new vetka;
-      kor->zn = znc;
-      kor->c = 1;
-      kor->levo = kor->pravo = nullptr;
-    } else if (kor->zn < znc) {
-        kor->levo = plusvetka(kor->levo, znc);
-      } else if (kor->zn > znc) {
-          kor->pravo = plusvetka(kor->pravo, znc);
-        } else {
-            kor->c++;
-          }
-    return kor;
+#include  <iostream>
+#include  <fstream>
+#include  <locale>
+#include  <cstdlib>
+#include  "bst.h"
+
+BST<std::string> makevetki(const char* filename) {
+  BST<std::string> vetki;
+  std::ifstream file(filename);
+  char t;
+  std::string slovo = "";
+    if (!file.is_open()) {
+        std::cout << "File error!" << std::endl;
+        return vetki;
     }
-    int poiskvetka(vetka* kor, const T& znc) {
-      if (kor == nullptr) {
-        return 0;
-      } else if (kor->zn == znc) {
-          return kor->c;
-        } else if (kor->zn < znc) {
-            return poiskvetka(kor->levo, znc);
-          } else {
-              return poiskvetka(kor->pravo, znc);
-            }
-    }
-    int dp(vetka* kor) {
-      int lft = 0, rht = 0;
-      if (kor == nullptr) {
-        return 0;
+    while (!file.eof()) {
+      t = file.get();
+      if ((t >= 'A') && (t <= 'Z'))
+        t += 32;
+      if ((t >= 'a') && (t <= 'z')) {
+        slovo += t;
       } else {
-          lft = dp(kor->levo);
-          rht = dp(kor->pravo);
-        }
-        if (rht > lft) {
-            return ++rht;
-        } else {
-            return ++lft;
-          }
+        vetki.Add(slovo);
+        slovo = "";
+      }
     }
-};
-#endif  // INCLUDE_BST_H_
+    file.close();
+  return vetki;
+}
